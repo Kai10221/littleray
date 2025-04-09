35ecc060991f103a5108db88ea8292dabcd5b931
Kai10221 <156641048+Kai10221@users.noreply.github.com>
2025-04-09 18:24:44 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,37 +4,48 @@ import requests, random, os
 from linebot import LineBotApi, WebhookHandler
 from linebot.models import MessageEvent, TextMessage, ImageSendMessage, TextSendMessage
 
-main = Flask(__name__)  # <-- 注意這裡
+main = Flask(__name__)
 
 line_bot_api = LineBotApi(os.getenv("LINE_CHANNEL_ACCESS_TOKEN"))
 handler = WebhookHandler(os.getenv("LINE_CHANNEL_SECRET"))
 
-@main.route("/")  # <-- 注意這裡
+@main.route("/")
 def home():
     return "Line Bot Running"
 
-@main.route("/callback", methods=["POST"])  # <-- 注意這裡
+@main.route("/callback", methods=["POST"])
 def callback():
     signature = request.headers["X-Line-Signature"]
     body = request.get_data(as_text=True)
 
     try:
         handler.handle(body, signature)
-    except:
+    except Exception as e:
+        print("Webhook 處理錯誤：", e)
         abort(400)
     return "OK"
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    if event.message.text == "抽圖":
+    if event.message.text.strip() == "抽圖":
         image_url = get_random_beauty_image()
+        print("抓到的圖片網址：", image_url)  # debug log
         if image_url:
-            msg = ImageSendMessage(original_content_url=image_url, preview_image_url=image_url)
+            msg = ImageSendMessage(
+                original_content_url=image_url,
+                preview_image_url=image_url
+            )
             line_bot_api.reply_message(event.reply_token, msg)
         else:
-            line_bot_api.reply_message(event.reply_token, TextSendMessage(text="目前沒抓到圖片，再試一次！"))
+            line_bot_api.reply_message(
+                event.reply_token,
+                TextSendMessage(text="沒抓到圖片，稍後再試～")
+            )
     else:
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(text="你說了：" + event.message.text))
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text="你說了：" + event.message.text)
+        )
 
 def get_random_beauty_image():
     base_url = "https://www.ptt.cc"
@@ -48,7 +59,14 @@ def get_random_beauty_image():
     for link in links:
         res = requests.get(link, headers={"cookie": "over18=1"})
         s = BeautifulSoup(res.text, "html.parser")
-        imgs = [img["href"] for img in s.select("a") if "imgur.com" in img.get("href", "")]
+        imgs = []
+        for a in s.select("a"):
+            href = a.get("href", "")
+            if "imgur.com" in href:
+                if href.startswith("https://imgur.com"):
+                    href = href.replace("https://imgur.com", "https://i.imgur.com") + ".jpg"
+                if href.startswith("http"):
+                    imgs.append(href)
         if imgs:
             return random.choice(imgs)
 
