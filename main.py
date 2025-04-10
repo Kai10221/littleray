c0f93b6121ab4997032b032afb6ecdb68f36fe79
Kai10221 <156641048+Kai10221@users.noreply.github.com>
2025-04-10 10:30:24 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,37 +4,29 @@ import requests, random, os
 from linebot import LineBotApi, WebhookHandler
 from linebot.models import MessageEvent, TextMessage, ImageSendMessage, TextSendMessage
 
-# 建立 Flask app 並設定 logger 的層級
 main = Flask(__name__)
-main.logger.setLevel("DEBUG")
 
 line_bot_api = LineBotApi(os.getenv("LINE_CHANNEL_ACCESS_TOKEN"))
 handler = WebhookHandler(os.getenv("LINE_CHANNEL_SECRET"))
 
 @main.route("/")
 def home():
-    main.logger.info("Home 被存取")
     return "Line Bot Running"
 
 @main.route("/callback", methods=["POST"])
 def callback():
     signature = request.headers.get("X-Line-Signature", "")
     body = request.get_data(as_text=True)
-    main.logger.info("收到 webhook 請求, Body: %s", body)
     try:
         handler.handle(body, signature)
-    except Exception as e:
-        main.logger.error("Webhook 處理錯誤: %s", e)
+    except:
         abort(400)
     return "OK"
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    main.logger.info("收到訊息: %s", event.message.text)
-    # 如果收到的訊息含有「抽」
     if "抽" in event.message.text:
         image_url = get_random_beauty_image()
-        main.logger.info("抓到的圖片網址: %s", image_url)
         if image_url:
             msg = ImageSendMessage(
                 original_content_url=image_url,
@@ -46,16 +38,12 @@ def handle_message(event):
                 event.reply_token,
                 TextSendMessage(text="沒抓到圖片，稍後再試～")
             )
-    else:
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text="你說了：" + event.message.text)
-        )
 
 def get_random_beauty_image():
     base_url = "https://www.ptt.cc"
-    # 使用方法1：先固定首頁，再搭配其他頁面
-    index_urls = [base_url + "/bbs/Beauty/index.html"] + [base_url + "/bbs/Beauty/index{}.html".format(i) for i in range(1, 3)]
+    # 合併首頁與其他頁
+    index_urls = [base_url + "/bbs/Beauty/index.html"] + \
+                 [base_url + "/bbs/Beauty/index{}.html".format(i) for i in range(1, 3)]
     article_links = []
     for url in index_urls:
         try:
@@ -63,8 +51,8 @@ def get_random_beauty_image():
             soup = BeautifulSoup(rs.text, "html.parser")
             links = [base_url + a["href"] for a in soup.select(".r-ent a") if a.get("href")]
             article_links.extend(links)
-        except Exception as e:
-            main.logger.error("取得文章連結失敗: %s, error: %s", url, e)
+        except:
+            continue
     random.shuffle(article_links)
     
     for link in article_links:
@@ -79,10 +67,8 @@ def get_random_beauty_image():
                         href = href.replace("https://imgur.com", "https://i.imgur.com") + ".jpg"
                     if href.startswith("http"):
                         imgs.append(href)
-            main.logger.info("檢查文章: %s, 圖片列表: %s", link, imgs)
             if imgs:
                 return random.choice(imgs)
-        except Exception as e:
-            main.logger.error("抓取文章失敗: %s, error: %s", link, e)
+        except:
             continue
     return None
