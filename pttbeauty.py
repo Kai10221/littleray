d8333e531e482e158a3f4087a5a78ee455c11034
Kai10221 <156641048+Kai10221@users.noreply.github.com>
2025-04-10 11:27:47 +0800

    Update pttbeauty.py

--- pttbeauty.py
+++ pttbeauty.py
@@ -4,7 +4,8 @@ from linebot.models import ImageSendMessage, TextSendMessage
 
 def handle_beauty(event, line_bot_api):
     image_url = get_random_beauty_image()
-    if image_url:
+    # 如果圖片連結不以 "https://" 開頭，則直接回覆文字（或你也可進行其他錯誤處理）
+    if image_url and image_url.startswith("https://"):
         msg = ImageSendMessage(
             original_content_url=image_url,
             preview_image_url=image_url
@@ -18,14 +19,15 @@ def handle_beauty(event, line_bot_api):
 
 def get_random_beauty_image():
     base_url = "https://www.ptt.cc"
-    # 抓取 Beauty 版文章，依序來自首頁與其他 2 頁
+    # 取得 Beauty 版首頁與其他兩頁
     index_urls = [base_url + "/bbs/Beauty/index.html"] + \
-                 [base_url + "/bbs/Beauty/index{}.html".format(i) for i in range(1, 10)]
+                 [base_url + "/bbs/Beauty/index{}.html".format(i) for i in range(1, 3)]
     article_links = []
     for url in index_urls:
         try:
             rs = requests.get(url, headers={"cookie": "over18=1"}, timeout=5)
             soup = BeautifulSoup(rs.text, "html.parser")
+            # 收集所有文章的連結
             links = [base_url + a["href"] for a in soup.select(".r-ent a") if a.get("href")]
             article_links.extend(links)
         except:
@@ -40,9 +42,14 @@ def get_random_beauty_image():
             for a in soup.select("a"):
                 href = a.get("href", "")
                 if "imgur.com" in href:
+                    # 如果網址以 "https://imgur.com" 開頭，轉成 "https://i.imgur.com" 加上副檔名
                     if href.startswith("https://imgur.com"):
                         href = href.replace("https://imgur.com", "https://i.imgur.com") + ".jpg"
-                    if href.startswith("http"):
+                    # 如果網址是以 "http://" 開頭，變成 https
+                    elif href.startswith("http://"):
+                        href = href.replace("http://", "https://")
+                    # 確保網址以 "https://" 開頭
+                    if href.startswith("https://"):
                         imgs.append(href)
             if imgs:
                 return random.choice(imgs)
