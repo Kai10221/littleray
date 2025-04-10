526a9b921624c1f9265ffa33abe09eaadbb4bd4c
Kai10221 <156641048+Kai10221@users.noreply.github.com>
2025-04-10 11:17:15 +0800

    Update pttbeauty.py

--- pttbeauty.py
+++ pttbeauty.py
@@ -1,47 +1,26 @@
-from flask import Flask, request, abort
 from bs4 import BeautifulSoup
-import requests, random, os
-from linebot import LineBotApi, WebhookHandler
-from linebot.models import MessageEvent, TextMessage, ImageSendMessage, TextSendMessage
+import requests, random
+from linebot.models import ImageSendMessage, TextSendMessage
 
-main = Flask(__name__)
-
-line_bot_api = LineBotApi(os.getenv("LINE_CHANNEL_ACCESS_TOKEN"))
-handler = WebhookHandler(os.getenv("LINE_CHANNEL_SECRET"))
-
-@main.route("/")
-def home():
-    return "Line Bot Running"
-
-@main.route("/callback", methods=["POST"])
-def callback():
-    signature = request.headers.get("X-Line-Signature", "")
-    body = request.get_data(as_text=True)
-    try:
-        handler.handle(body, signature)
-    except:
-        abort(400)
-    return "OK"
-
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    if "抽" in event.message.text:
-        image_url = get_random_beauty_image()
-        if image_url:
-            msg = ImageSendMessage(
-                original_content_url=image_url,
-                preview_image_url=image_url
-            )
-            line_bot_api.reply_message(event.reply_token, msg)
-        else:
-            line_bot_api.reply_message(
-                event.reply_token,
-                TextSendMessage(text="沒抓到圖片，稍後再試～")
-            )
+def handle_beauty(event, line_bot_api):
+    image_url = get_random_beauty_image()
+    if image_url:
+        msg = ImageSendMessage(
+            original_content_url=image_url,
+            preview_image_url=image_url
+        )
+        line_bot_api.reply_message(event.reply_token, msg)
+    else:
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text="沒抓到圖片，稍後再試～")
+        )
 
 def get_random_beauty_image():
     base_url = "https://www.ptt.cc"
-    index_urls = [base_url + "/bbs/Beauty/index.html"] + [base_url + "/bbs/Beauty/index{}.html".format(i) for i in range(1, 3)]
+    # 抓取 Beauty 版文章，依序來自首頁與其他 2 頁
+    index_urls = [base_url + "/bbs/Beauty/index.html"] + \
+                 [base_url + "/bbs/Beauty/index{}.html".format(i) for i in range(1, 3)]
     article_links = []
     for url in index_urls:
         try:
