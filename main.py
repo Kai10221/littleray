142e22c5e42324852e03031cbb7905c180ffe46d
Kai10221 <156641048+Kai10221@users.noreply.github.com>
2025-04-10 11:06:45 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,72 +1,52 @@
 from flask import Flask, request, abort
-from bs4 import BeautifulSoup
-import requests, random, os
 from linebot import LineBotApi, WebhookHandler
-from linebot.models import MessageEvent, TextMessage, ImageSendMessage, TextSendMessage
+from linebot.models import MessageEvent, TextMessage
 
-main = Flask(__name__)
+import os
+
+# 匯入各功能模組
+from pttbeauty import handle_beauty
+from ptt_hot import get_hot_articles, make_flex_carousel
+
+app = Flask(__name__)
 
 line_bot_api = LineBotApi(os.getenv("LINE_CHANNEL_ACCESS_TOKEN"))
 handler = WebhookHandler(os.getenv("LINE_CHANNEL_SECRET"))
 
-@main.route("/")
+@app.route("/")
 def home():
     return "Line Bot Running"
 
-@main.route("/callback", methods=["POST"])
+@app.route("/callback", methods=["POST"])
 def callback():
     signature = request.headers.get("X-Line-Signature", "")
     body = request.get_data(as_text=True)
+
     try:
         handler.handle(body, signature)
-    except:
+    except Exception as e:
+        print("LINE Webhook 發生錯誤：", e)
         abort(400)
     return "OK"
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    if "抽" in event.message.text:
-        image_url = get_random_beauty_image()
-        if image_url:
-            msg = ImageSendMessage(
-                original_content_url=image_url,
-                preview_image_url=image_url
-            )
-            line_bot_api.reply_message(event.reply_token, msg)
-        else:
-            line_bot_api.reply_message(
-                event.reply_token,
-                TextSendMessage(text="沒抓到圖片，稍後再試～")
-            )
+    text = event.message.text
 
-def get_random_beauty_image():
-    base_url = "https://www.ptt.cc"
-    index_urls = [base_url + "/bbs/Beauty/index.html"] + [base_url + "/bbs/Beauty/index{}.html".format(i) for i in range(1, 3)]
-    article_links = []
-    for url in index_urls:
-        try:
-            rs = requests.get(url, headers={"cookie": "over18=1"}, timeout=5)
-            soup = BeautifulSoup(rs.text, "html.parser")
-            links = [base_url + a["href"] for a in soup.select(".r-ent a") if a.get("href")]
-            article_links.extend(links)
-        except:
-            continue
-    random.shuffle(article_links)
-    
-    for link in article_links:
-        try:
-            res = requests.get(link, headers={"cookie": "over18=1"}, timeout=5)
-            soup = BeautifulSoup(res.text, "html.parser")
-            imgs = []
-            for a in soup.select("a"):
-                href = a.get("href", "")
-                if "imgur.com" in href:
-                    if href.startswith("https://imgur.com"):
-                        href = href.replace("https://imgur.com", "https://i.imgur.com") + ".jpg"
-                    if href.startswith("http"):
-                        imgs.append(href)
-            if imgs:
-                return random.choice(imgs)
-        except:
-            continue
-    return None
+    # 功能 1：抽圖（轉交給 pttbeauty 模組處理）
+    if "抽" in text:
+        handle_beauty(event, line_bot_api)
+
+    # 功能 2：PTT 熱門文章
+    elif any(kw in text for kw in ["熱門", "推文", "排行"]):
+        articles = get_hot_articles()
+        flex_msg = make_flex_carousel(articles)
+        line_bot_api.reply_message(event.reply_token, flex_msg)
+
+    # 其他訊息回覆
+    else:
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextMessage(text="你說了：" + text)
+        )
+
